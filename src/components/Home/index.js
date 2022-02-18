633ba6242754f2f989ca62d1084f4e8b719ee935
Vivek <vivekp6828@gmail.com>
2022-02-18 13:37:15 +0000

    home page constructed with all the functionalities given

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,229 @@
-const Home = () => <h1>Home Route</h1>
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Slider from 'react-slick'
+import Loader from 'react-loader-spinner'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import UsersPosts from '../UsersPosts'
+
+const dataFetchStatusConstants = {
+  initial: 'INITIAL',
+  loading: 'LOADING',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+}
+
+class Home extends Component {
+  state = {
+    storiesFetchStatus: dataFetchStatusConstants.initial,
+    userStories: [],
+    postsFetchStatus: dataFetchStatusConstants.initial,
+    usersPosts: [],
+  }
+
+  componentDidMount() {
+    const jwtToken = Cookies.get('jwtToken')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    this.getUserStories(jwtToken, options)
+    this.getUsersPosts(jwtToken, options)
+  }
+
+  getUserStories = async (jwtToken, options) => {
+    this.setState({storiesFetchStatus: dataFetchStatusConstants.loading})
+    const response = await fetch(
+      'https://apis.ccbp.in/insta-share/stories',
+      options,
+    )
+
+    if (response.ok) {
+      const data = await response.json()
+      const userStories = data.users_stories
+      this.setState({storiesFetchStatus: dataFetchStatusConstants.success})
+      this.setState({userStories})
+    }
+    if (!response.ok) {
+      this.setState({storiesFetchStatus: dataFetchStatusConstants.failure})
+    }
+  }
+
+  getUsersPosts = async (jwtToken, options) => {
+    this.setState({postsFetchStatus: dataFetchStatusConstants.loading})
+    const response = await fetch(
+      'https://apis.ccbp.in/insta-share/posts',
+      options,
+    )
+    if (response.ok) {
+      const data = await response.json()
+      const usersPosts = data.posts
+      this.setState({postsFetchStatus: dataFetchStatusConstants.success})
+      this.setState({usersPosts})
+    }
+    if (!response.ok) {
+      this.setState({postsFetchStatus: dataFetchStatusConstants.failure})
+    }
+  }
+
+  renderUserStories = () => {
+    const {userStories, storiesFetchStatus} = this.state
+    const settings = {
+      dots: true,
+
+      slidesToScroll: 1,
+    }
+    switch (storiesFetchStatus) {
+      case dataFetchStatusConstants.loading:
+        return (
+          <div
+            className="loader-container"
+            testid="loader"
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Loader type="TailSpin" color="#4094EF" height={50} width={50} />
+          </div>
+        )
+      case dataFetchStatusConstants.success:
+        return (
+          <div>
+            <div
+              style={{
+                backgroundColor: 'blue',
+                display: 'flex',
+                flexDirection: 'Column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              {/* for small screen */}
+              <Slider
+                {...settings}
+                slidesToShow={4}
+                style={{
+                  width: '92%',
+                }}
+              >
+                {userStories.map(each => (
+                  <div key={each.user_id}>
+                    <div>
+                      <div>
+                        <img src={each.story_url} alt="user story" />
+                        <p>{each.user_name}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </Slider>
+
+              {/* for large screens */}
+              <Slider
+                {...settings}
+                slidesToShow={7}
+                style={{
+                  width: '92%',
+                }}
+              >
+                {userStories.map(each => (
+                  <div key={each.user_id}>
+                    <div>
+                      <div>
+                        <img src={each.story_url} alt="user story" />
+                        <p>{each.user_name}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
+        )
+      case dataFetchStatusConstants.failure:
+        return (
+          <div>
+            <div>
+              <img
+                src="https://res.cloudinary.com/duqlsmi22/image/upload/v1645180684/alert-triangle-failure-view_htbcnn.png"
+                alt="failure-view"
+              />
+            </div>
+            <p>Something went wrong. Please try again</p>
+            <button type="button">Retry</button>
+          </div>
+        )
+      default:
+        return null
+    }
+  }
+
+  renderUsersPosts = () => {
+    const {usersPosts, postsFetchStatus} = this.state
+    console.log(usersPosts)
+    return (
+      <ul>
+        {usersPosts.map(
+          eachPost => (
+            /* const postDetails = eachPost.post_details
+          console.log(postDetails) */
+            <UsersPosts key={eachPost.post_id} userPost={eachPost} />
+          ),
+          /*  return (
+            <li key={eachPost.post_id}>
+              <div>
+                <img src={eachPost.profile_pic} alt="post author profile" />
+                <p>{eachPost.user_name}</p>
+              </div>
+              <div>
+                <img src={postDetails.image_url} alt="post" />
+              </div>
+              <div>
+                <button type="button">
+                  <FcLike testid="unLikeIcon" />
+                </button>
+                <button type="button">
+                  <BsHeart testid="likeIcon" />
+                </button>
+                <button type="button">
+                  <FaRegComment />
+                </button>
+                <button type="button">
+                  <BiShareAlt />
+                </button>
+              </div>
+              <p>{eachPost.likes_count} likes</p>
+              <p>{postDetails.caption}</p>
+              <ul>
+                {eachPost.comments.map(eachComment => (
+                  <li key={eachComment.user_id}>
+                    <p>{eachComment.user_name}</p>
+                    <p>{eachComment.comment}</p>
+                  </li>
+                ))}
+              </ul>
+              <p>{eachPost.created_at}</p>
+            </li>
+          ) */
+        )}
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderUserStories()}
+        {this.renderUsersPosts()}
+      </div>
+    )
+  }
+}
 
 export default Home
---
+++ src/components/UsersPosts/index.js
@@ -0,0 +1,108 @@
+import {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {FcLike} from 'react-icons/fc'
+import {BsHeart} from 'react-icons/bs'
+import {FaRegComment} from 'react-icons/fa'
+import {BiShareAlt} from 'react-icons/bi'
+import Cookies from 'js-cookie'
+
+class UsersPosts extends Component {
+  constructor(props) {
+    super(props)
+    const {userPost} = props
+    this.state = {postLikedStatus: false, postLikedCounts: userPost.likes_count}
+  }
+
+  likeCLicked = async () => {
+    const {userPost} = this.props
+    const jwtToken = Cookies.get('jwtToken')
+    const options = {
+      method: 'POST',
+      headers: {Authorization: `Bearer ${jwtToken}`},
+      body: JSON.stringify({like_status: true}),
+    }
+    const response = await fetch(
+      `https://apis.ccbp.in/insta-share/posts/${userPost.post_id}/like`,
+      options,
+    )
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+    /* this.setState({postLikedStatus:true}) */
+    this.setState(prevState => ({
+      postLikedStatus: true,
+      postLikedCounts: prevState.postLikedCounts + 1,
+    }))
+  }
+
+  unlikeCLicked = async () => {
+    const {userPost} = this.props
+    const jwtToken = Cookies.get('jwtToken')
+    const options = {
+      method: 'POST',
+      headers: {Authorization: `Bearer ${jwtToken}`},
+      body: JSON.stringify({like_status: false}),
+    }
+    const response = await fetch(
+      `https://apis.ccbp.in/insta-share/posts/${userPost.post_id}/like`,
+      options,
+    )
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+    /* this.setState({postLikedStatus:false}) */
+    this.setState(prevState => ({
+      postLikedStatus: false,
+      postLikedCounts: prevState.postLikedCounts - 1,
+    }))
+  }
+
+  render() {
+    const {userPost} = this.props
+    const postDetails = userPost.post_details
+    const {postLikedCounts, postLikedStatus} = this.state
+
+    return (
+      <li>
+        <div>
+          <img src={userPost.profile_pic} alt="post author profile" />
+          <Link to={`/users/${userPost.user_id}`}>
+            <p>{userPost.user_name}</p>
+          </Link>
+        </div>
+        <div>
+          <img src={postDetails.image_url} alt="post" />
+        </div>
+        <div>
+          {!postLikedStatus ? (
+            <button type="button" onClick={this.likeCLicked}>
+              <FcLike testid="unLikeIcon" />
+            </button>
+          ) : (
+            <button onClick={this.unlikeCLicked} type="button">
+              <BsHeart testid="likeIcon" />
+            </button>
+          )}
+          <button type="button">
+            <FaRegComment />
+          </button>
+          <button type="button">
+            <BiShareAlt />
+          </button>
+        </div>
+        <p>{postLikedCounts} likes</p>
+        <p>{postDetails.caption}</p>
+        <ul>
+          {userPost.comments.map(eachComment => (
+            <li key={eachComment.user_id}>
+              <p>{eachComment.user_name}</p>
+              <p>{eachComment.comment}</p>
+            </li>
+          ))}
+        </ul>
+        <p>{userPost.created_at}</p>
+      </li>
+    )
+  }
+}
+export default UsersPosts
